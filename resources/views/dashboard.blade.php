4dc141f0ee93ffb5f809e8972bb681a77cb32201
Willem Van Bockstal <willem@spatie.be>
2016-10-06 15:23:40 +0200

    Fix border; Group css properties

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,24 +5,22 @@
     @javascript(compact('pusherKey'))
 
     <div class="dashboard" id="dashboard">
+            <google-calendar grid="a1:a2"></google-calendar>
 
-        <google-calendar grid="a1:a2"></google-calendar>
+            <last-fm grid="b1:c1"></last-fm>
 
-        <last-fm grid="b1:c1"></last-fm>
+            <current-time grid="d1" dateformat="ddd DD/MM"></current-time>
 
-        <current-time grid="d1" dateformat="ddd DD/MM"></current-time>
+            <packagist-statistics grid="b2"></packagist-statistics>
 
-        <packagist-statistics grid="b2"></packagist-statistics>
+            <rain-forecast grid="c2"></rain-forecast>
 
-        <rain-forecast grid="c2"></rain-forecast>
+            <internet-connection grid="d2"></internet-connection>
 
-        <internet-connection grid="d2"></internet-connection>
-
-        <github-file file-name="freek" grid="a3"></github-file>
-        <github-file file-name="rogier" grid="b3"></github-file>
-        <github-file file-name="seb" grid="c3"></github-file>
-        <github-file file-name="willem" grid="d3"></github-file>
-
+            <github-file file-name="freek" grid="a3"></github-file>
+            <github-file file-name="rogier" grid="b3"></github-file>
+            <github-file file-name="seb" grid="c3"></github-file>
+            <github-file file-name="willem" grid="d3"></github-file>
     </div>
 
 @endsection
