1955f7c0a600ceaa49059daa6e1f6eaf2602a0c6
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2016-10-27 12:26:00 +0200

    RainForecast refactor

--- resources/assets/js/components/RainForecast.vue
+++ resources/assets/js/components/RainForecast.vue
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-import _ from 'lodash';
+import { filter, map, sumBy } from 'lodash';
 import Echo from '../mixins/echo';
 import Graph from './graph';
 import Grid from './grid';
@@ -36,21 +36,35 @@ export default {
 
     computed: {
         status() {
-            if (this.noRainPredicted()) {
+            if (this.noRainPredicted === true) {
                 return 'dry';
             }
 
-            if (this.nothingButRainPredicted()) {
+            if (this.nothingButRainPredicted === true) {
                 return 'wet';
             }
 
             return 'rainy';
         },
+        noRainPredicted() {
+            const rainScore = sumBy(this.forecast, foreCastItem => {
+                return parseInt(foreCastItem.chanceOfRain);
+            });
+
+            return rainScore === 0;
+        },
+        nothingButRainPredicted() {
+            const foreCastItemWithNoRain = filter(this.forecast, foreCastItem => {
+                return foreCastItem.chanceOfRain < 40;
+            }).length;
+
+            return foreCastItemWithNoRain.length === 0;
+        },
         graphLabels() {
-            return _.map(this.forecast, 'minutes');
+            return map(this.forecast, 'minutes');
         },
         graphData() {
-            return _.map(this.forecast, 'chanceOfRain');
+            return map(this.forecast, 'chanceOfRain');
         },
         graphPeriod() {
             return this.forecast[this.forecast.length - 1].minutes;
@@ -74,20 +88,6 @@ export default {
         },
         getSavedStateId() {
             return 'rain-forecast-update';
-        },
-        noRainPredicted() {
-            const rainScore = _.sumBy(this.forecast, foreCastItem => {
-                return parseInt(foreCastItem.chanceOfRain);
-            });
-
-            return rainScore === 0;
-        },
-        nothingButRainPredicted() {
-            const foreCastItemWithNoRain = _.filter(this.forecast, foreCastItem => {
-                return foreCastItem.chanceOfRain < 40;
-            }).length;
-
-            return foreCastItemWithNoRain.length === 0;
         },
     },
 };
