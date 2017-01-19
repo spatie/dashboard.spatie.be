f49f50df5cbdc422181193c7de8cf435dba3bdd5
freek <freek@spatie.be>
2017-01-19 17:39:54 +0100

    commit

--- resources/assets/js/helpers.js
+++ resources/assets/js/helpers.js
@@ -22,7 +22,7 @@ export function addClassModifiers(base, modifiers = []) {
 export function relativeDate(value) {
     const date = moment(value);
 
-    const diffInDays = date.diff(moment(), 'days');
+    const diffInDays = moment().diff(date, 'days');
 
     if (diffInDays < 7) {
         return date.calendar();
@@ -34,38 +34,37 @@ export function relativeDate(value) {
 export function relativeDateTime(value) {
     const date = moment(value);
 
-    if (date.diff(moment(), 'days') > 1) {
-        return `${date.diff(moment(), 'days')} days ago`;
+    if (moment().diff(date, 'days') > 1) {
+        return `${moment().diff(date, 'days')} days ago`;
     }
 
-    if (date.diff(moment(), 'hours') > 24) {
+    if (moment().diff(date, 'hours') > 24) {
         return "A day ago"
     }
 
-    if (date.diff(moment(), 'hours') > 1) {
-        return `${date.diff(moment(), 'hours')} hours ago`;
+    if (moment().diff(date, 'hours') > 1) {
+        return `${moment().diff(date, 'hours')} hours ago`;
     }
 
-    if (date.diff(moment(), 'minutes') > 59) {
+    if (moment().diff(date, 'minutes') > 59) {
         return 'An hour ago';
     }
 
-    if (date.diff(moment(), 'seconds') > 119) {
-        return `${date.diff(moment(), 'minutes')} minutes ago`;
+    if (moment().diff(date, 'seconds') > 119) {
+        return `${moment().diff(date, 'minutes')} minutes ago`;
     }
 
-    if (date.diff(moment(), 'seconds') >= 60) {
+    if (moment().diff(date, 'seconds') >= 60) {
         return 'A minute ago';
     }
 
-    if (date.diff(moment(), 'seconds') > 10) {
-        return `${date.diff(moment(), 'seconds')} seconds ago`;
+    if (moment().diff(date, 'seconds') > 10) {
+        return `${moment().diff(date, 'seconds')} seconds ago`;
     }
-console.log(`${date.diff(moment(), 'seconds')} seconds ago`);
+
     return 'Just now';
 }
 
 export function diffInSeconds(otherMoment) {
-    console.log(otherMoment);
     return moment().diff(otherMoment, 'seconds')
 }
