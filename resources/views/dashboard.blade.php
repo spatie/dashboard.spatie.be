6cf95182f620e18e728ccba08fdcacee5dc5fa0a
Freek Van der Herten <freek@spatie.be>
2017-01-20 11:51:09 +0100

    commit (#21)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,7 +5,7 @@
     @javascript(compact('pusherKey'))
 
     <div class="dashboard" id="dashboard">
-        <twitter grid="a1:a3"></twitter>
+        <twitter initial-tweets="{{ json_encode($initialTweets) }}" grid="a1:a3"></twitter>
         <google-calendar grid="b1:b2"></google-calendar>
         <rain-forecast grid="b3"></rain-forecast>
         <github-file file-name="freek" grid="c1"></github-file>
@@ -16,6 +16,7 @@
         <last-fm grid="d3:e3"></last-fm>
         <current-time grid="e1" dateformat="ddd DD/MM"></current-time>
         <packagist-statistics grid="e2"></packagist-statistics>
+
     </div>
 
 @endsection
