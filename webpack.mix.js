441e7ac4f31d11bfd663aa87d1118a6c4efb0fe9
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2017-02-03 09:37:02 +0100

    Mix (#33)

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,15 +1,26 @@
-let mix = require('laravel-mix').mix;
+const { mix } = require('laravel-mix');
+const { NormalModuleReplacementPlugin } = require('webpack');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+    .js('resources/assets/js/app.js', 'public/js')
+    .sass('resources/assets/sass/app.scss', 'public/css')
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .version()
+
+    .webpackConfig({
+        plugins: [
+            // For some reason, laravel-echo is trying to require jquery. To
+            // ignore this requirement, we'll replace jquery with a noop
+            // function.
+            new NormalModuleReplacementPlugin(
+                /jquery/,
+                'node-noop'
+            ),
+        ],
+
+        stats: {
+            // The "pretty" errors sometimes lack information. To display full
+            // stack traces, run `DEBUG=1 yarn run dev`.
+            errors: process.env.DEBUG,
+        },
+    });
