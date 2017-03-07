d54e07d0e0973a52ea786506a729c580a3bbec64
Willem Van Bockstal <willem@spatie.be>
2017-03-07 10:16:19 +0100

    Rearrange tiles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,13 +6,12 @@
 
     <div class="dashboard" id="dashboard">
         <twitter :initial-tweets="{{ json_encode($initialTweets) }}" grid="a1:a3"></twitter>
-        <google-calendar grid="b1:b2"></google-calendar>
-        <rain-forecast grid="b3"></rain-forecast>
+        <google-calendar grid="b1:b3"></google-calendar>
         <github-file file-name="freek" grid="c1"></github-file>
-        <github-file file-name="seb" grid="d1"></github-file>
-        <github-file file-name="rogier" grid="c2"></github-file>
-        <github-file file-name="willem" grid="d2"></github-file>
-        <uptime-monitor grid="c3"></uptime-monitor>
+        <github-file file-name="seb" grid="c2"></github-file>
+        <github-file file-name="willem" grid="c3"></github-file>
+        <rain-forecast grid="d1"></rain-forecast>
+        <uptime-monitor grid="d2"></uptime-monitor>
         <last-fm grid="d3:e3"></last-fm>
         <current-time grid="e1" dateformat="ddd DD/MM"></current-time>
         <packagist-statistics grid="e2"></packagist-statistics>
