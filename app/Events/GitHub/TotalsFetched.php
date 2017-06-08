980b044f8fd86f129bc785779277b9ac52bf8c6f
freek <freek@spatie.be>
2017-06-08 08:28:22 +0200

    add docblock

--- app/Events/GitHub/TotalsFetched.php
+++ app/Events/GitHub/TotalsFetched.php
@@ -6,14 +6,19 @@ use App\Events\DashboardEvent;
 
 class TotalsFetched extends DashboardEvent
 {
+    /** @var int */
     public $stars;
 
+    /** @var int */
     public $issues;
 
+    /** @var int */
     public $pullRequests;
 
+    /** @var int */
     public $contributors;
 
+    /** @var int */
     public $numberOfRepos;
 
     public function __construct(array $totals)
