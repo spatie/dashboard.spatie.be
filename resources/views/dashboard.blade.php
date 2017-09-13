c10ee308581a46c1c5ac8b89c2ccf2935dbbf573
Willem Van Bockstal <willem@spatie.be>
2017-09-13 10:41:13 +0200

    Add team members

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,21 +4,20 @@
 
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
-<dashboard id="dashboard" columns="5" rows="3">
+<dashboard id="dashboard" columns="6" rows="3">
     <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
-    <calendar position="b1:b2"></calendar>
-    <music position="c1:d1"></music>
-    <uptime position="b3"></uptime>
-
+    <calendar position="b1:b3"></calendar>
+    <uptime position="c1"></uptime>  
+    <music position="d1:e1"></music>
     <tasks team-member="alex" position="c2"></tasks>
-    <tasks team-member="freek" position="d2"></tasks>
-    <tasks team-member="seb" position="c3"></tasks>
-    <tasks team-member="willem" position="d3"></tasks>
-
-    <time-weather position="e1" date-format="ddd DD/MM"></time-weather>
-    <packagist position="e2"></packagist>
-    <github position="e3"></github>
-
+    <tasks team-member="brent" position="d2"></tasks>
+    <tasks team-member="freek" position="e2"></tasks>
+    <tasks team-member="harish" position="c3"></tasks>
+    <tasks team-member="seb" position="d3"></tasks>
+    <tasks team-member="willem" position="e3"></tasks>
+    <time-weather position="f1" date-format="ddd DD/MM"></time-weather>
+    <packagist position="f2"></packagist>
+    <github position="f3"></github>
     <internet-connection></internet-connection>
 </dashboard>
 
