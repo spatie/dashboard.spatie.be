62e6bc4bdf7918e94c3e238d90111b942df7601a
Arne De Smedt <arne.desmedt@vanin.be>
2017-10-06 11:39:51 +0200

    feature - add 3 github projects to the dashboard
    Add 3 github projects to the dashboard and clean all the other tiles, except for the weather

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,21 +4,12 @@
 
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
-<dashboard id="dashboard" columns="6" rows="3">
-    <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
-    <uptime position="a1:a3"></uptime>  
-    <packagist position="b1:b2"></packagist>
-    <github position="b3"></github>
-    <music position="c1:d1"></music>
-    <tasks team-member="alex" position="c2"></tasks>
-    <tasks team-member="brent" position="d2"></tasks>
-    <tasks team-member="freek" position="e2"></tasks>
-    <tasks team-member="harish" position="c3"></tasks>
-    <tasks team-member="seb" position="d3"></tasks>
-    <tasks team-member="willem" position="e3"></tasks>
-    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Asia/Kolkata" weather-city="Mira-Bhayandar"></time-weather>
-    <time-weather position="f1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
-    <calendar position="f2:f3"></calendar>
+<dashboard id="dashboard" columns="2" rows="3">
+    {{--<uptime position="a1:a3"></uptime>--}}
+    <github-project position="a1" project="SOL2"></github-project>
+    <github-project position="a2" project="SOL"></github-project>
+    <github-project position="a3" project="SOL-management"></github-project>
+    <time-weather position="b1" date-format="DD/MM/YYYY" time-format="HH:mm:ss" time-zone="Europe/Brussels" weather-city="Gent"></time-weather>
     <internet-connection></internet-connection>
 </dashboard>
 
