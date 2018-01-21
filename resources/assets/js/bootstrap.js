d4552e38eccdc1a4a9e12c73a3f95e8d76b1e516
Robbert <RobLui@users.noreply.github.com>
2018-01-21 11:00:06 +0100

    rm typo (#97)
    Minor typo

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -13,5 +13,5 @@ moment.updateLocale('en', {
 });
 
 // Pusher is forcing us to globally register the library. Bug? Shouldn't be
-// necessary, hopefully we can remove this ar some point.
+// necessary, hopefully we can remove this at some point.
 window.Pusher = require('pusher-js');
