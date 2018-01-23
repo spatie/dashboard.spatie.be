688f26c9478c31d7c3fc92e5a5539481961b5096
Freek <freek@spatie.be>
2018-01-23 14:58:57 +0100

    remove logging

--- resources/assets/js/mixins/echo.js
+++ resources/assets/js/mixins/echo.js
@@ -3,10 +3,9 @@ import { forIn } from 'lodash';
 export default {
     created() {
         forIn(this.getEventHandlers(), (handler, eventName) => {
-            console.log('subscribed', eventName);
             this.$root.echo
                 .private('dashboard')
-                .listen(`.App.Events.${eventName}`, response => (console.log('heard', eventName), handler(response)));
+                .listen(`.App.Events.${eventName}`, response => handler(response));
         });
     },
 };
