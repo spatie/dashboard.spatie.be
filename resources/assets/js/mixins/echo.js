81d4064ac71fba4bdd02a0670774ddc657045d3f
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2018-02-02 11:20:17 +0100

    Update event namespace

--- resources/assets/js/mixins/echo.js
+++ resources/assets/js/mixins/echo.js
@@ -5,7 +5,7 @@ export default {
         forIn(this.getEventHandlers(), (handler, eventName) => {
             this.$root.echo
                 .private('dashboard')
-                .listen(`.App.Events.${eventName}`, response => handler(response));
+                .listen(eventName, response => handler(response));
         });
     },
 };
