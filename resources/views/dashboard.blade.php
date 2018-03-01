7de4c1e5919eb5337092e0827299ad89be706f53
Willem Van Bockstal <willem@spatie.be>
2018-03-01 15:25:17 +0100

    Update team

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,22 +4,20 @@
 
 @javascript(compact('pusherKey', 'pusherCluster', 'usingNodeServer'))
 
-<dashboard id="dashboard" columns="6" rows="3">
+<dashboard id="dashboard" columns="5" rows="3">
     <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a3"></twitter>
     <uptime position="a1:a3"></uptime>
     <packagist position="b1"></packagist>
     <npm position="b2"></npm>
     <github position="b3"></github>
-    <music position="c1:d1"></music>
-    <tasks team-member="alex" position="c2"></tasks>
-    <tasks team-member="brent" position="d2"></tasks>
-    <tasks team-member="freek" position="e2"></tasks>
-    <tasks team-member="harish" position="c3"></tasks>
-    <tasks team-member="seb" position="d3"></tasks>
-    <tasks team-member="willem" position="e3"></tasks>
-    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Asia/Kolkata" weather-city="Mira-Bhayandar"></time-weather>
-    <time-weather position="f1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
-    <calendar position="f2:f3"></calendar>
+    <music position="d1"></music>
+    <tasks team-member="alex" position="c1"></tasks>
+    <tasks team-member="brent" position="c2"></tasks>
+    <tasks team-member="freek" position="c3"></tasks>
+    <tasks team-member="seb" position="d2"></tasks>
+    <tasks team-member="willem" position="d3"></tasks>
+    <time-weather position="e1" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
+    <calendar position="e2:e3"></calendar>
     <internet-connection></internet-connection>
 </dashboard>
 
