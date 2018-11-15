3bb7c02e671698528f5cbadb54cc380ffb4c7876
freek <freek@spatie.be>
2018-11-15 09:03:43 +0100

    update deps

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -6,11 +6,6 @@ use Illuminate\Cookie\Middleware\EncryptCookies as BaseEncrypter;
 
 class EncryptCookies extends BaseEncrypter
 {
-    /**
-     * The names of the cookies that should not be encrypted.
-     *
-     * @var array
-     */
     protected $except = [
 
     ];
