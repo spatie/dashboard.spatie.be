b2595fea76797051013089986883ec44f2bc68f4
freek <freek@spatie.be>
2018-11-15 12:50:52 +0100

    commit

--- app/Console/Components/Dashboard/DetermineAppearanceCommand.php
+++ app/Console/Components/Dashboard/DetermineAppearanceCommand.php
@@ -21,7 +21,7 @@ class DetermineAppearanceCommand extends Command
     public function handle()
     {
         $appearance = $this->sunIsUp()
-            ? 'dark-mode'
+            ? 'light-mode'
             : 'dark-mode';
 
         event(new UpdateAppearance($appearance));
