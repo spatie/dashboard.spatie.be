70ebab180f8fc1808279fda14c39e8c9918087ad
freek <freek@spatie.be>
2018-11-15 10:03:26 +0100

    simplify model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,22 +7,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    protected $guarded = [];
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
