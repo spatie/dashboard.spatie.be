24951f215c69e6fd5e804c4993e53c671e625d10
freek <freek@spatie.be>
2018-11-15 10:39:19 +0100

    rename nampespace

--- app/Http/Controllers/UpdateTemperatureController.php
+++ app/Http/Controllers/UpdateTemperatureController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Events\Temperature\TemperatureFetched;
+use App\Events\TimeWeather\TemperatureFetched;
 
 class UpdateTemperatureController
 {
