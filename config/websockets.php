9f42ca90a1703e7d109943eb3ca912fa344d3a24
Ruben Van Assche <ruben@spatie.be>
2018-12-20 15:17:35 +0100

    Disable websocket statistics

--- config/websockets.php
+++ config/websockets.php
@@ -18,7 +18,7 @@ return [
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'enable_client_messages' => false,
-            'enable_statistics' => true,
+            'enable_statistics' => false,
         ],
     ],
 
