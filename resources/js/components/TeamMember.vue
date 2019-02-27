f2f8eb2bae04acaa84d1c29c480876de222c47c2
Willem Van Bockstal <willem@spatie.be>
2019-02-27 16:38:48 +0100

    Truncate tasks

--- resources/js/components/TeamMember.vue
+++ resources/js/components/TeamMember.vue
@@ -1,65 +1,65 @@
 <template>
-    <tile :position="position">
-        <div
-            class="grid gap-padding h-full markup"
-            :style="tasks ? 'grid-template-rows: auto 1fr' : 'grid-template-rows: 1fr'"
-        >
-            <div
-                class="grid gap-2 items-center w-full bg-tile z-10"
-                style="grid-template-columns: auto 1fr"
-            >
-                <div
-                    v-if="artwork != ''"
-                    class="overflow-hidden w-10 h-10 rounded border border-screen"
-                >
-                    <component :is="trackUrl ? 'a' : 'span'" :href="trackUrl || ''" target="_blank">
-                        <img :src="artwork" class="w-10 h-10" />
-                    </component>
-                </div>
-                <div v-else>
-                    <avatar :src="avatar" />
-                    <div
-                        v-if="isBirthDay"
-                        class="absolute flex items-center jsutify-center text-3xl"
-                        v-html="emoji('👑')"
-                        style="top: -1rem; right: .05rem; transform:rotate(7deg);"
-                    />
-                </div>
-                <div class="leading-tight min-w-0">
-                    <h2 class="truncate capitalize">
-                        {{ displayName || name }}
-                        <span
-                            v-if="statusEmoji != ''"
-                            class="text-xl"
-                            v-html="emoji(statusEmoji)"
-                        />
-                    </h2>
-                    <component
-                        v-if="currentTrack != ''"
-                        class="truncate text-sm block"
-                        :is="trackUrl ? 'a' : 'span'"
-                        :href="trackUrl || ''"
-                        target="_blank"
-                    >
-                        <span v-html="emoji('🎵')" /> {{ currentTrack }}
-                    </component>
-                </div>
-            </div>
-            <div class="align-self-center" v-if="tasks.length">
-                <ul>
-                    <li v-for="task in tasks" :key="task.id">
-                        <p>
-                            {{ task.project }}
-                            <span v-if="task.name" class="text-xs text-dimmed">
-                                <br>{{ task.name }}
-                            </span>
-                        </p>
-                        <span class="ml-2 font-bold variant-tabular">{{ task.formatted_time }}</span>
-                    </li>
-                </ul>
-            </div>
+  <tile :position="position">
+    <div
+      class="grid gap-padding h-full markup"
+      style="grid-template-columns: 100%"
+      :style="tasks ? 'grid-template-rows: auto 1fr' : 'grid-template-rows: 1fr'"
+    >
+      <div
+        class="grid gap-2 items-center w-full bg-tile z-10"
+        style="grid-template-columns: auto 1fr"
+      >
+        <div v-if="artwork != ''" class="overflow-hidden w-10 h-10 rounded border border-screen">
+          <component :is="trackUrl ? 'a' : 'span'" :href="trackUrl || ''" target="_blank">
+            <img :src="artwork" class="w-10 h-10">
+          </component>
         </div>
-    </tile>
+        <div v-else>
+          <avatar :src="avatar"/>
+          <div
+            v-if="isBirthDay"
+            class="absolute flex items-center jsutify-center text-3xl"
+            v-html="emoji('👑')"
+            style="top: -1rem; right: .05rem; transform:rotate(7deg);"
+          />
+        </div>
+        <div class="leading-tight min-w-0">
+          <h2 class="truncate capitalize">
+            {{ displayName || name }}
+            <span
+              v-if="statusEmoji != ''"
+              class="text-xl"
+              v-html="emoji(statusEmoji)"
+            />
+          </h2>
+          <component
+            v-if="currentTrack != ''"
+            class="truncate text-sm block"
+            :is="trackUrl ? 'a' : 'span'"
+            :href="trackUrl || ''"
+            target="_blank"
+          >
+            <span v-html="emoji('🎵')"/>
+            {{ currentTrack }}
+          </component>
+        </div>
+      </div>
+      <div class="align-self-center" v-if="tasks.length">
+        <ul>
+          <li v-for="task in tasks" :key="task.id">
+            <p class="truncate">
+              {{ task.project }}
+              <span v-if="task.name" class="text-xs text-dimmed">
+                <br>
+                {{ task.name }}
+              </span>
+            </p>
+            <span class="ml-2 font-bold variant-tabular">{{ task.formatted_time }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </tile>
 </template>
 
 <script>
