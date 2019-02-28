d1ca9b0528d1aafd8520445f505f469683312ec9
Freek Van der Herten <freek@spatie.be>
2019-02-28 14:27:49 +0100

    Laravel 5.8 Shift (#139)
    * Adopt PSR-2 coding style
    
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer
    
    * Shift configuration
    
    * Shift deprecated helpers
    
    Laravel recommends using the `Str` and `Arr` class methods directly instead of the respective helper functions. These helper functions are [deprecated in Laravel 5.8][1] and will be removed in a future version.
    
    [1]: https://github.com/laravel/framework/pull/26898
    
    * Shift Laravel dependencies

--- config/logging.php
+++ config/logging.php
@@ -1,6 +1,7 @@
 <?php
 
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\SyslogUdpHandler;
 
 return [
 
@@ -35,7 +36,8 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['daily'],
+            'ignore_exceptions' => false,
         ],
 
         'single' => [
@@ -48,7 +50,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
-            'days' => 7,
+            'days' => 14,
         ],
 
         'slack' => [
@@ -59,9 +61,20 @@ return [
             'level' => 'critical',
         ],
 
+        'papertrail' => [
+            'driver' => 'monolog',
+            'level' => 'debug',
+            'handler' => SyslogUdpHandler::class,
+            'handler_with' => [
+                'host' => env('PAPERTRAIL_URL'),
+                'port' => env('PAPERTRAIL_PORT'),
+            ],
+        ],
+
         'stderr' => [
             'driver' => 'monolog',
             'handler' => StreamHandler::class,
+            'formatter' => env('LOG_STDERR_FORMATTER'),
             'with' => [
                 'stream' => 'php://stderr',
             ],
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 /*
@@ -20,6 +21,6 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
-        'remember_token' => str_random(10),
+        'remember_token' => Str::random(10),
     ];
 });
