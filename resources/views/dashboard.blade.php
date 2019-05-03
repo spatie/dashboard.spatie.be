52ef4640f10718b3fa6eb26327c0aa1ab65ad26e
Rias <hello@rias.be>
2019-05-03 11:13:27 +0200

    Trains have higher priority than team tiles

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,6 @@
 <div id="dashboard">
     <dashboard class="font-sans">
         <twitter :initial-tweets="{{ json_encode($initialTweets) }}" position="a1:a16"></twitter>
-        <tile-timer on="16:00" off="19:00">
-            <trains position="a1:a24"></trains>
-        </tile-timer>
         <team-member name="adriaan" avatar="{{ gravatar('adriaan@spatie.be') }}" birthday="1995-10-22" position="b1:b8"></team-member>
         <team-member name="alex" avatar="{{ gravatar('alex@spatie.be') }}" birthday="1996-02-05" position="c1:c8"></team-member>
         <team-member name="brent" avatar="{{ gravatar('brent@spatie.be') }}" birthday="1994-07-30" position="b9:b16"></team-member>
@@ -19,6 +16,9 @@
         <team-member name="jef" avatar="{{ gravatar('jef@spatie.be') }}" birthday="1975-03-28" position="d11:d13"></team-member>
         <team-member name="wouter" avatar="{{ gravatar('wouter@spatie.be') }}" birthday="1991-03-15" position="d14:d16"></team-member>
         <team-member name="willem" avatar="{{ gravatar('willem@spatie.be') }}" birthday="1975-09-04" position="d17:d24"></team-member>
+        <tile-timer on="16:00" off="19:00">
+            <trains position="a1:a24"></trains>
+        </tile-timer>
         <time-weather position="e1:e6" date-format="ddd DD/MM" time-zone="Europe/Brussels" weather-city="Antwerp"></time-weather>
         <internet-connection position="e1:e6"></internet-connection>
         <statistics position="d1:d10"></statistics>
