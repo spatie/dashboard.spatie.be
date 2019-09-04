6247d70cc6d11eb9fbc3d9cc30f95497f606895f
Laravel Shift <shift@laravelshift.com>
2019-09-04 09:22:31 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files
    easier, Shift defaulted them so you can review the commit diff for
    changes. Moving forward, you should use ENV variables or create a
    separate config file to allow the core config files to remain
    automatically upgradeable.

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('BROADCAST_DRIVER', 'pusher'),
+    'default' => env('BROADCAST_DRIVER', 'null'),
 
     /*
     |--------------------------------------------------------------------------
@@ -36,9 +36,8 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'host' => '127.0.0.1',
-                'port' => 6001,
-                'scheme' => 'http'
+                'cluster' => env('PUSHER_APP_CLUSTER'),
+                'useTLS' => true,
             ],
         ],
 
