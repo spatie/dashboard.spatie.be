bc2b3ba3b5677eaf5dc49d8377974a55e6422404
Mark Walet <mark.walet@gmail.com>
2020-04-10 10:17:40 +0200

    Fix type hinting for collection

--- app/Console/Components/Statistics/FetchPackagistTotalsCommand.php
+++ app/Console/Components/Statistics/FetchPackagistTotalsCommand.php
@@ -4,6 +4,7 @@ namespace App\Console\Components\Statistics;
 
 use GuzzleHttp\Client;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
 use Spatie\Packagist\Packagist;
 use App\Events\Statistics\PackagistTotalsFetched;
 
@@ -18,18 +19,17 @@ class FetchPackagistTotalsCommand extends Command
         $this->info('Fetching packagist totals...');
 
         $packagist = new Packagist(new Client());
-
         $totals = collect($packagist->getPackagesByVendor(config('services.packagist.vendor'))['packageNames'])
                 ->map(function ($packageName) use ($packagist) {
                     return $packagist->findPackageByName($packageName)['package'];
                 })
-                ->pipe(function ($packageProperties) {
+                ->pipe(function (Collection $packageProperties) {
                     return [
                         'monthly' => $packageProperties->sum('downloads.monthly'),
                         'total' => $packageProperties->sum('downloads.total'),
                     ];
                 });
-
+        dd($totals);
         event(new PackagistTotalsFetched($totals));
 
         $this->info('All done!');
