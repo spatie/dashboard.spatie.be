d2d4ef6f72ca6254ee445922a4a75d32f5af3071
freek <freek@spatie.be>
2020-04-20 16:45:44 +0200

    wip

--- resources/views/components/tiles/uptime.blade.php
+++ resources/views/components/tiles/uptime.blade.php
@@ -1,15 +1,10 @@
-<div wire:poll.5s>
-    @if(count($downSites))
-        <x-dashboard-tile :position="$position">
-            <div class="grid gap-padding h-full markup" style="grid-template-rows: auto 1fr" class="markup bg-warn">
-                <h1>Downtime</h1>
-                <ul class="align-self-center">
-                    @foreach($downSites as $downSite)
-                        <div class="font-bold truncate">{{ $downSite }}</div>
-                    @endforeach
-                </ul>
-            </div>
-        </x-dashboard-tile>
-    @endif
-</div>
-
+<x-dashboard-tile wire:poll.30s :position="$position" :show="count($downSites)">
+    <div class="grid gap-padding h-full markup" style="grid-template-rows: auto 1fr" class="markup bg-warn">
+        <h1>Downtime</h1>
+        <ul class="align-self-center">
+            @foreach($downSites as $downSite)
+                <div class="font-bold truncate">{{ $downSite }}</div>
+            @endforeach
+        </ul>
+    </div>
+</x-dashboard-tile>
