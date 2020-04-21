fd9ce5bebd573eeece5e3f532a8a5cd2a27ddf4c
freek <freek@spatie.be>
2020-04-21 18:18:54 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Spatie\BelgianTrainsTile\BelgianTrainsTileComponent;
 use Spatie\OhDearUptimeTile\OhDearUptimeTileComponent;
 
 class AppServiceProvider extends ServiceProvider
@@ -10,5 +11,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         OhDearUptimeTileComponent::showTile(fn (array $downSites) => count($downSites) > 0);
+        BelgianTrainsTileComponent::showTile(fn () => now()->hour >= 16 && now()->hour <= 21);
     }
 }
