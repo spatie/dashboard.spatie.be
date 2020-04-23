6c411b6792061d65e5e60ff366625e33e44bf569
freek <freek@spatie.be>
2020-04-24 00:10:18 +0200

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,7 +10,7 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-       // OhDearUptimeTileComponent::showTile(fn (array $downSites) => count($downSites));
-       // BelgianTrainsTileComponent::showTile(fn () => now()->hour >= 16 && now()->hour <= 20);
+        OhDearUptimeTileComponent::showTile(fn (array $downSites) => count($downSites));
+        BelgianTrainsTileComponent::showTile(fn () => now()->hour >= 16 && now()->hour <= 20);
     }
 }
