2e254e4d89f0ae3f54d6fb4f0355d1dfb1afae11
Freek Van der Herten <freek@spatie.be>
2020-04-28 12:23:46 +0200

    Update dashboard.php

--- config/dashboard.php
+++ config/dashboard.php
@@ -1,8 +1,20 @@
 <?php
 
 return [
+    /*
+     * The dashboard supports these themes:
+     *
+     * - light: always use light mode
+     * - dark: always use dark mode
+     * - device: follow the OS preference for determining light or dark mode
+     * - auto: use light mode when the sun is up, dark mode when the sun is down
+     */
     'theme' => 'auto',
 
+    /*
+     * When the dashboard uses the `auto` theme, these coordinates will be used
+     * to determine whether the sun is up or down
+     */
     'auto_theme_location' => [
         'lat' => 51.260197,
         'lng' => 4.402771,
