51aa8c77c3771f18cbdd2ac8de6363fb2bc23ea6
freek <freek@spatie.be>
2020-05-22 20:42:22 +0200

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,1 @@
-<x-dashboard>
-    {{-- <livewire:calendar-tile position="a1" refresh-in-seconds="5" /> --}}
-    <livewire:twitter-tile position="b1:b3" />
-    <livewire:time-weather-tile position="a1" />
-</x-dashboard>
+Empty view
