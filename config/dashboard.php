282a613f9aab14522418a5f00e347408ef16bb20
freek <freek@spatie.be>
2020-05-22 21:39:01 +0200

    wip

--- config/dashboard.php
+++ config/dashboard.php
@@ -9,7 +9,7 @@ return [
      * - device: follow the OS preference for determining light or dark mode
      * - auto: use light mode when the sun is up, dark mode when the sun is down
      */
-    'theme' => 'auto',
+    'theme' => 'light',
 
     /*
      * When the dashboard uses the `auto` theme, these coordinates will be used
