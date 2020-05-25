f84ae7c5b0eda9ae2928b0eafb3a422b5e434968
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-05-25 12:50:47 +0200

    Revert "wip"
    This reverts commit 282a613f9aab14522418a5f00e347408ef16bb20.

--- config/dashboard.php
+++ config/dashboard.php
@@ -9,7 +9,7 @@ return [
      * - device: follow the OS preference for determining light or dark mode
      * - auto: use light mode when the sun is up, dark mode when the sun is down
      */
-    'theme' => 'light',
+    'theme' => 'auto',
 
     /*
      * When the dashboard uses the `auto` theme, these coordinates will be used
