45d53f6330f6e1512407ffd1ecf7c6bb40238f6d
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-05-25 12:51:46 +0200

    Revert "wip"
    This reverts commit 51aa8c77c3771f18cbdd2ac8de6363fb2bc23ea6.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,5 @@
-Empty view
+<x-dashboard>
+    {{-- <livewire:calendar-tile position="a1" refresh-in-seconds="5" /> --}}
+    <livewire:twitter-tile position="b1:b3" />
+    <livewire:time-weather-tile position="a1" />
+</x-dashboard>
