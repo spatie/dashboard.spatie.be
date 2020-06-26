af5ed9ba8d656c5e745e7a60ad0c7c10f2b02a54
Alex Vanderbist <alex.vanderbist@gmail.com>
2020-06-26 15:15:38 +0200

    Update deps

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,11 @@
 <?php
 
-use App\Http\Middleware\AccessToken;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\UpdateTemperatureController;
-use App\Http\Controllers\UpdateIndoorAirQualityController;
+use App\Http\Middleware\AccessToken;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => AccessToken::class], function () {
     Route::view('/', 'dashboard');
-
-    Route::post('indoor-air-quality', UpdateIndoorAirQualityController::class);
 });
 
 Route::ohDearWebhooks('/oh-dear-webhooks');
