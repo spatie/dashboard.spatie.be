7ef0b5e06a670c66c12a6e6928869a085a89483f
Laravel Shift <shift@laravelshift.com>
2020-09-10 06:28:22 +0000

    Shift cleanup

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ class UserFactory extends Factory
     {
         static $password;
 
-    return [
+        return [
         'name' => $this->faker->name,
         'email' => $this->faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
