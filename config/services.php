cba73aa5a23589802462109e4f3f2324d7a12f7e
Ruben Van Assche <ruben@spatie.be>
2021-12-07 12:02:36 +0100

    Update composer

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
 
     'last-fm' => [
         'api_key' => env('LAST_FM_API_KEY'),
-        'users' => explode(',', env('LAST_FM_USERS')),
+        'users' => explode(',', env('LAST_FM_USERS') ?? ''),
     ],
 
     'packagist' => [
