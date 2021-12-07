f99eb0ac86e7e41a6ef20add55e2ad5de4f50254
Ruben Van Assche <ruben@spatie.be>
2021-12-07 15:39:10 +0100

    wip

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,100 +1,174 @@
+
 @setup
-{{--$server = 'dashboard.spatie.be';--}}
-$server = '157.230.125.153';
-$userAndServer = "forge@{$server}";
-$siteName = 'dashboard.spatie.be';
-$pathOnServer = '/home/forge/' . $siteName;
-$deploymentId = 'Deployment of ' . $siteName . ':' . $pathOnServer . ' by ' . get_current_user(). ':';
+$server = "dashboard.spatie.be";
+$userAndServer = 'forge@157.230.125.153';
+$repository = "rubenvanassche/ella-ruben.be";
+$baseDir = "/home/forge/{$server}";
+$releasesDir = "{$baseDir}/releases";
+$currentDir = "{$baseDir}/current";
+$newReleaseName = date('Ymd-His');
+$newReleaseDir = "{$releasesDir}/{$newReleaseName}";
+$user = get_current_user();
+
+function logMessage($message) {
+return "echo '\033[32m" .$message. "\033[0m';\n";
+}
 @endsetup
 
-@servers(['web' => $userAndServer, 'localhost' => '127.0.0.1'])
+@servers(['local' => '127.0.0.1', 'remote' => $userAndServer])
 
-@task('display start message', ['on' => 'localhost'])
-echo 'start deploying on {{ $server }}. Path: {{ $pathOnServer }}'
-@endtask
+@macro('deploy')
+startDeployment
+cloneRepository
+runComposer
+runYarn
+generateAssets
+updateSymlinks
+optimizeInstallation
+migrateDatabase
+blessNewRelease
+cleanOldReleases
+restart pi
+finishDeploy
+@endmacro
 
-@task('checkout master branch', ['on' => 'localhost'])
-echo 'checking out the master branch'
+@macro('deploy-code')
+deployOnlyCode
+@endmacro
+
+@macro('deploy-clean')
+cloneRepository
+runComposerWithDependencies
+runYarn
+generateAssets
+updateSymlinks
+optimizeInstallation
+blessNewRelease
+cleanOldReleases
+restart pi
+finishDeploy
+@endmacro
+
+@task('startDeployment', ['on' => 'local'])
+{{ logMessage("🏃  Starting deployment...") }}
 git checkout master
-@endtask
-
-@task('bring app down', ['on' => 'web'])
-echo 'bringing app down'
-cd '{{ $pathOnServer }}'
-php artisan down
-@endtask
-
-@task('pull changes on server', ['on' => 'web'])
-cd '{{ $pathOnServer }}'
 git pull origin master
 @endtask
 
-@task('run composer install', ['on' => 'web'])
-echo 'running composer install'
-cd '{{ $pathOnServer }}'
-composer install --prefer-dist --no-scripts -q -o
-php artisan cache:clear
+@task('cloneRepository', ['on' => 'remote'])
+{{ logMessage("🌀  Cloning repository...") }}
+[ -d {{ $releasesDir }} ] || mkdir {{ $releasesDir }};
+cd {{ $releasesDir }};
+
+# Create the release dir
+mkdir {{ $newReleaseDir }};
+
+# Clone the repo
+GIT_SSH_COMMAND='ssh -i /home/forge/.ssh/id_deploy' git clone --depth 1 git@github.com:{{ $repository }} {{ $newReleaseName }}
+
+# Configure sparse checkout
+cd {{ $newReleaseDir }}
+git config core.sparsecheckout true
+echo "*" > .git/info/sparse-checkout
+echo "!storage" >> .git/info/sparse-checkout
+echo "!public/build" >> .git/info/sparse-checkout
+git read-tree -mu HEAD
+
+# Mark release
+cd {{ $newReleaseDir }}
+echo "{{ $newReleaseName }}" > public/release-name.txt
 @endtask
 
-@task('run yarn', ['on' => 'web'])
-echo 'running yarn'
-cd '{{ $pathOnServer }}'
+@task('runComposer', ['on' => 'remote'])
+{{ logMessage("🚚  Running Composer...") }}
+cd {{ $newReleaseDir }};
+composer install --prefer-dist --no-scripts --no-dev -q -o;
+{{--composer install --prefer-dist --no-scripts -q -o;--}}
+@endtask
+
+@task('runComposerWithDependencies', ['on' => 'remote'])
+{{ logMessage("🚚  Running Composer...") }}
+cd {{ $newReleaseDir }};
+composer install;
+@endtask
+
+@task('runYarn', ['on' => 'remote'])
+{{ logMessage("📦  Running Yarn...") }}
+cd {{ $newReleaseDir }};
 yarn config set ignore-engines true
-yarn
+yarn --frozen-lockfile
 @endtask
 
-@task('generate assets', ['on' => 'web'])
-echo 'generating assets'
-cd '{{ $pathOnServer }}'
+@task('generateAssets', ['on' => 'remote'])
+{{ logMessage("🌅  Generating assets...") }}
+cd {{ $newReleaseDir }};
 yarn run production
 @endtask
 
-@task('bring app up', ['on' => 'web'])
-cd '{{ $pathOnServer }}'
-echo 'bringing app up'
-php artisan up
+@task('updateSymlinks', ['on' => 'remote'])
+{{ logMessage("🔗  Updating symlinks to persistent data...") }}
+# Remove the storage directory and replace with persistent data
+rm -rf {{ $newReleaseDir }}/storage;
+cd {{ $newReleaseDir }};
+ln -nfs {{ $baseDir }}/persistent/storage storage;
+
+rm -rf {{ $newReleaseDir }}/public/storage;
+cd {{ $newReleaseDir }}/public/;
+ln -nfs {{ $baseDir }}/persistent/storage/app/public storage;
+
+# Import the environment config
+cd {{ $newReleaseDir }};
+ln -nfs {{ $baseDir }}/.env .env;
 @endtask
 
-@task('reload php', ['on' => 'web'])
-sudo service php7.4-fpm restart
-sudo supervisorctl restart all
+@task('optimizeInstallation', ['on' => 'remote'])
+{{ logMessage("✨  Optimizing installation...") }}
+cd {{ $newReleaseDir }};
+php artisan clear-compiled;
+@endtask
 
-cd '{{ $pathOnServer }}'
-php artisan cache:clear
-php artisan view:clear
+@task('migrateDatabase', ['on' => 'remote'])
+{{ logMessage("🙈  Migrating database...") }}
+cd {{ $newReleaseDir }};
+php artisan migrate --force;
+@endtask
+
+
+@task('blessNewRelease', ['on' => 'remote'])
+{{ logMessage("🙏  Blessing new release...") }}
+ln -nfs {{ $newReleaseDir }} {{ $currentDir }};
+cd {{ $newReleaseDir }}
 php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+
+sudo service php8.1-fpm restart
+sudo supervisorctl restart all
 @endtask
 
-@task('display success message', ['on' => 'localhost'])
-echo "application successfully deployed"
+@task('cleanOldReleases', ['on' => 'remote'])
+{{ logMessage("🚾  Cleaning up old releases...") }}
+# Delete all but the 5 most recent.
+cd {{ $releasesDir }}
+ls -dt {{ $releasesDir }}/* | tail -n +6 | xargs -d "\n" sudo chown -R forge .;
+ls -dt {{ $releasesDir }}/* | tail -n +6 | xargs -d "\n" rm -rf;
 @endtask
 
 @task('restart pi', ['on' => 'localhost'])
 ssh pi 'sudo reboot'
 @endtask
 
-@task('deployOnlyCode',['on' => 'web'])
-cd {{ $pathOnServer }}
-git pull origin master
+@task('finishDeploy', ['on' => 'local'])
+{{ logMessage("🚀  Application deployed!") }}
 @endtask
 
-@macro('deploy')
-display start message
-checkout master branch
-bring app down
-pull changes on server
-run composer install
-run yarn
-generate assets
-reload php
-bring app up
-restart pi
-display success message
-@endmacro
-
-@macro('deploy-code')
-deployOnlyCode
-reload php
-{{-- restart pi --}}
-@endmacro
-
+@task('deployOnlyCode',['on' => 'remote'])
+{{ logMessage("💻  Deploying code changes...") }}
+cd {{ $currentDir }}
+GIT_SSH_COMMAND='ssh -i /home/forge/.ssh/id_deploy' git pull origin master
+php artisan config:clear
+php artisan cache:clear
+php artisan config:cache
+sudo supervisorctl restart all
+sudo service php8.1-fpm restart
+@endtask
