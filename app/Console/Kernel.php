ac2a8ab4af2619b8db25b3388edaf321d49c9335
Rias <hey@rias.be>
2024-01-26 11:15:11 +0100

    Don't fetch tracks anymore

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(FetchBelgianTrainsCommand::class)->everyMinute();
         $schedule->command(FetchCalendarEventsCommand::class)->everyMinute();
         $schedule->command(FetchAttendancesCommand::class)->everyFiveMinutes();
-        $schedule->command(FetchCurrentTracksCommand::class)->everyMinute();
+        //$schedule->command(FetchCurrentTracksCommand::class)->everyMinute();
         $schedule->command(FetchBuienradarForecastsCommand::class)->everyFiveMinutes();
         $schedule->command(FetchOpenWeatherMapDataCommand::class)->everyFiveMinutes();
         //$schedule->command(FetchTasksCommand::class)->everyFiveMinutes();
