01ea194417ce06277742512855b84606ca36c26e
Tim Van Dijck <tim@spatie.be>
2026-03-16 10:59:47 +0100

    Add spatie.be

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,8 +26,9 @@
     <livewire:calendar-tile position="e7:e20" :calendar-id="config('google-calendar.calendar_id')" />
     
     <livewire:fathom-tile position="b1:b8" siteId="GSENXMLW" label="📯 Mailcoach" />
-    <livewire:fathom-tile position="c1:C8" siteId="LBABKDJB" label="🎆 Flare" />
-    <livewire:statistics-tile position="d1:d8" />
+    <livewire:fathom-tile position="c1:c8" siteId="LBABKDJB" label="🎆 Flare" />
+    <livewire:fathom-tile position="d1:d8" siteId="OMNDKUTR" label="🔵 Spatie" />
+    <livewire:statistics-tile position="b9:b13" />
 
     <livewire:oh-dear-uptime-tile position="e7:e16" />
 
